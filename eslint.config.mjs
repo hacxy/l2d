5e30525c8d966fd3c9d08a3a66c8591ff2f7aeee
hacxy <hacxy.js@outlook.com>
2025-07-22 17:14:38 +0800

    chore: change eslint config

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,8 @@
-import defineLint from '@hacxy/eslint-config/vue';
+import defineLint from '@hacxy/eslint-config';
 
 export default defineLint({
   rules: {
     'max-params': 0,
-    'vue/block-lang': 0,
-    'vue/enforce-style-attribute': 0
   },
   ignores: ['lib/**', 'dist/**', 'docs/api/**', 'docs/guide/**'],
 });
