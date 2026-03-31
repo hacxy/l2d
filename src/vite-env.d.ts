2f40e830711cb4c0ff5203d5b384751957a5871b
hacxy <hacxy.js@outlook.com>
2026-03-31 17:23:19 +0800

    refactor: 调整目录结构，vite版本升级为V8

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,9 +1,2 @@
 /// <reference types="vite/client" />
-
-declare global {
-  interface Window {
-    PIXI: typeof import('pixi.js')
-  }
-}
-
 export {};
--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,24 @@
-import { defineConfig } from 'vite';
+import { createLogger, defineConfig } from 'vite';
 import dts from 'vite-plugin-dts';
 
-export default defineConfig(() => ({
+const logger = createLogger();
+const loggerWarn = logger.warn;
+logger.warn = (msg, options) => {
+  // 忽略nodejs模块警告
+  if (msg.includes('has been externalized for browser compatibility'))
+    return;
+  loggerWarn(msg, options);
+};
+
+export default defineConfig({
+  customLogger: logger,
   build: {
     lib: {
       name: 'LIVE2D',
       entry: 'src/index.ts',
       formats: ['es', 'iife'],
-      fileName: format => `index.${format === 'iife' ? 'min.js' : 'js'}`
+      fileName: format => `index.${format === 'iife' ? 'min.js' : 'js'}`,
     },
   },
-  plugins: [dts({ rollupTypes: true })]
-}));
+  plugins: [dts()]
+});
